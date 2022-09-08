afb1d1d9f02a9f2db8c232dbf53ebde1770a151b
Matthew Woodward <matthew_woodward@saa.senate.gov>
2022-09-08 10:43:25 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    @{{ user.username }} - {{ fullName }}
+  </div>
+</template>
+
+<script>
+import { computed, defineComponent, reactive, ref } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const followers = ref(0)
+    const user = reactive({
+      id: 1,
+      username: 'mpwoodward',
+      firstName: 'Matt',
+      lastName: 'Woodward',
+      email: 'mpwoodward@gmail.com',
+      isAdmin: true,
+    })
+
+    const fullName = computed(() => {
+      return `${user.firstName} ${user.lastName}`
+    })
+
+    return {
+      followers,
+      user,
+      fullName,
+    }
+  },
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
