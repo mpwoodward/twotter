6372af689743c5431502e4439436f82104ad6999
Matthew Woodward <matthew_woodward@saa.senate.gov>
2022-09-08 14:43:26 -0700

    form to add new twoots

--- src/user/UserProfile.vue
+++ src/user/UserProfile.vue
@@ -7,6 +7,27 @@
       <div class="user-profile__follower-count">
         <strong>Followers:</strong> {{ followers }}
       </div>
+      <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
+        <label for="newTwoot"><strong>New Twoot</strong></label>
+        <textarea id="newToot" rows="4" v-model="newTwootContent" />
+
+        <div class="user-profile__create-twoot-type">
+          <label for="newTwootType"><strong>Type: </strong></label>
+          <select id="newTwootType" v-model="selectedTwootType">
+            <option
+              :value="option.value"
+              v-for="(option, index) in twootTypes"
+              :key="index"
+            >
+              {{ option.name }}
+            </option>
+          </select>
+        </div>
+
+        <button>
+          Twoot!
+        </button>
+      </form>
     </div>
     <div class="user-profile__twoots-wrapper">
       <TwootItem
@@ -50,6 +71,18 @@ const user = reactive({
     },
   ],
 })
+const twootTypes = [
+  {
+    value: 'draft',
+    name: 'Draft'
+  },
+  {
+    value: 'instant',
+    name: 'Instant Twoot'
+  },
+]
+const newTwootContent = ref('')
+const selectedTwootType = ref('instant')
 
 const fullName = computed(() => `${user.firstName} ${user.lastName}`)
 
@@ -57,6 +90,16 @@ const followUser = () => followers.value++
 
 const toggleFavorite = (id: number) => {
   console.log(`Favorited Twoot ${id}!`)
+}
+
+const createNewTwoot = () => {
+  if (newTwootContent.value && selectedTwootType.value !== 'draft') {
+    user.twoots.unshift({
+      id: user.twoots.length + 1,
+      content: newTwootContent.value,
+    })
+    newTwootContent.value = ''
+  }
 }
 
 watch(followers, (newFollowerCount, oldFollowerCount) => {
@@ -95,6 +138,12 @@ onMounted(() => {
   margin-right: auto;
   padding: 0 10px;
   font-weight: bold;
+}
+
+.user-profile__create-twoot {
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
 }
 
 h1 {
